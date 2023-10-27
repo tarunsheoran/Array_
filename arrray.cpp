49731097e28ce8e2ae6e8210b6e569fe9595f3bb
Tarun Sheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-10-27 21:00:25 +0530

    Add files via upload

---
+++ arrray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int arr[12]={1,2,4,3,3,22,663,44,32,12};
+	cout<<"VAlue at index 6 is = "<<arr[6]<<endl<<endl;
+	
+	
+	
+	int new_arr[]={12,43,23};
+	int n=5;
+	cout<<"Printing The Array = ";
+	for(int i=0;i<n;i++){
+		cout<<new_arr[i]<<" ";
+	}
+}
